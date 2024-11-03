3527763e21ee29685638c9b54bac1ab81cc637c8
Vinicius Moreira da Silva <VINI.MOREIRA13@hotmail.com>
2024-11-02 21:17:05 -0300

    Ajustes para permitir a inclusão dos parametros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { DOCUMENT } from '@angular/common';
+import { FormBuilder } from '@angular/forms';
 
 import uitoolkit from "@zoom/videosdk-ui-toolkit";
 
@@ -11,27 +12,20 @@ import uitoolkit from "@zoom/videosdk-ui-toolkit";
 })
 export class AppComponent {
 
-  sessionContainer: any;
-  authEndpoint = ''
-  inSession: boolean = false
-  config = {
-    videoSDKJWT: '',
-    sessionName: 'test',
-    userName: 'Angular',
-    sessionPasscode: '123',
-    features: ['preview', 'video', 'audio', 'settings', 'users', 'chat', 'share'],
-    options: { init: {}, audio: {}, video: {}, share: {}},
-    virtualBackground: {
-       allowVirtualBackground: true,
-       allowVirtualBackgroundUpload: true,
-       virtualBackgrounds: ['https://images.unsplash.com/photo-1715490187538-30a365fa05bd?q=80&w=1945&auto=format&fit=crop']
-    }
-  };
-  role = 1
-
-  constructor(public httpClient: HttpClient, @Inject(DOCUMENT) document: any) {
+  constructor(public httpClient: HttpClient, @Inject(DOCUMENT) document: any, private formBuilder: FormBuilder,) {
 
   }
+
+  sessionContainer: any;
+  authEndpoint = 'https://slick-donkeys-tap.loca.lt'
+  inSession: boolean = false
+
+  sessionName: string = '';
+  userName: string = '';
+  password: string = '';
+  role: number = 0;
+  config: any ;
+  
 
   ngOnInit() {
     
@@ -42,6 +36,22 @@ export class AppComponent {
 
     this.inSession = true
     
+    this.config = {
+      videoSDKJWT: '',
+      sessionName: this.sessionName,
+      userName: this.userName,
+      sessionPasscode: this.password,
+      features: ['preview', 'video', 'audio', 'settings', 'users', 'chat', 'share'],
+      options: { init: {}, audio: {}, video: {}, share: {}},
+      virtualBackground: {
+         allowVirtualBackground: true,
+         allowVirtualBackgroundUpload: true,
+        // virtualBackgrounds: ['https://images.unsplash.com/photo-1715490187538-30a365fa05bd?q=80&w=1945&auto=format&fit=crop']
+      },
+      role: this.role
+    };
+    
+  
     this.httpClient.post(this.authEndpoint, {
 	    sessionName:  this.config.sessionName,
       role: this.role,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -11,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
